384ee0b1837db33a01f355ce612e9ec20ef95fbe
F.Hoeborn <f.hoeborn@googlemail.com>
2013-02-26 20:22:01 +0100

    Changed Main

--- FirstSteps/src/interpreter/Main.java
+++ FirstSteps/src/interpreter/Main.java
@@ -1,65 +1,72 @@
 package interpreter;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PushbackReader;
+import java.util.List;
+
 import lexer.Lexer;
+import lexer.LexerException;
 import node.Start;
 import parser.Parser;
+import parser.ParserException;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Run, run, run!");
 
-		try {
-			System.in.read();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		if (args.length < 1) {
+			System.out.println("No filename supplied!");
 		}
+
 		ToC interp = null;
+
+		Lexer lexer = null;
 		try {
-			Lexer lexer = new Lexer(new PushbackReader(new FileReader(
-					"testCodeFS")
-			// new InputStreamReader(System.in)
-			// new StringReader("1?2:3 ")
-					));
-			System.out.println("Lex, lex!");
-			Parser parser = new Parser(lexer);
+			lexer = new Lexer(new PushbackReader(new FileReader(args[0])));
+		} catch (FileNotFoundException e) {
+			System.out.println("Error occured: File not found!");
+			return;
+		}
+		System.out.println("Lex, lex!");
+		Parser parser = new Parser(lexer);
+
+		Start ast = null;
+
+		System.out.println("Versuch mal zu parsen.");
+		try {
 			System.out.println("Pars, pars!");
-			Start ast;
-
-			System.out.println("Versuch mal zu parsen.");
 			ast = parser.parse();
-			System.out.println("Der Parser parst");
-
+		} catch (ParserException e) {
+			System.out
+					.println("Parser Error occured! Error: " + e.getMessage());
+			return;
+		} catch (LexerException e) {
+			System.out.println("Lexer Error occured! Error: " + e.getMessage());
+			return;
+		} catch (IOException e) {
+			System.out.println("IO error occured! Error: " + e.getMessage());
+			return;
+		}
+		try {
 			/* Get our Interpreter going. */
 			interp = new ToC("C:/users/fhoeborn", "olaf");
 			ast.apply(interp);
-			//ast.apply(interp);
-			
-			System.out.println(interp.output.toString());
-			System.out.println("FunctionScope: "
-					+ interp.currentFunctionVariableScope.toString());
-			System.out.println("GlobalScope: "
-					+ interp.currentGlobalVariableScope.toString());
-			System.out.println("StructScope: "
-					+ interp.currentStructVariableScope.toString());		
-
-			// System.out.print(interp.cp.code.toString());
-
-		} catch (Exception e) {
-			System.out.println("Verkackt!");
-			if (interp != null) {
-				System.out.println("FunctionScope: "
-						+ interp.currentFunctionVariableScope.toString());
-				System.out.println("GlobalScope: "
-						+ interp.currentGlobalVariableScope.toString());
-				System.out.println("StructScope: "
-						+ interp.currentStructVariableScope.toString());
-			}
-
-			e.printStackTrace();
+		} catch (SemanticException e) {
+			System.out.println("Semantic Error occured!\nError: "
+					+ e.getMessage());
+			return;
 		}
 
+		printWarnings(interp.getWarnings());
+
+		System.out.println("Compile completed!");
+	}
+
+	private static void printWarnings(List<String> warnings) {
+		for (String warning : warnings) {
+			System.out.println("Warning: " + warning);
+		}
 	}
 }
