459ecf9e63fcbe43d74e2dd7c56a3c106b9ad888
Benjamin Arnold <b.arnold88@gmx.de>
2013-02-26 20:06:30 +0100

    120

--- FirstSteps/src/interpreter/ToC.java
+++ FirstSteps/src/interpreter/ToC.java
@@ -34,7 +34,7 @@ public class ToC extends DepthFirstAdapter {
 	@Override
 
 	public void caseAPrintImpl(APrintImpl node) {
-		output.append("printf(\"%d\n\",");
+		output.append("printf(\"%d\\n\",");
 		node.getExpr().apply(this);
 		output.append(");\n");
 		node.getImpl().apply(this);
